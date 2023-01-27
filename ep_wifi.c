88ae1e789e919206dfa104dc285669060a32dead
Abdellah Radad <123770144+abdellah2288@users.noreply.github.com>
2023-01-27 20:55:40 +0100

    Add files via upload

---
+++ ep_wifi.c
@@ -0,0 +1,148 @@
+/*
+ *		Author: Abdellah Radad 
+ *		Licensed under GPL V2
+ *	This esp-idf component handles networking on the esp32, for our purposes	being able to connect to a network and discover access points is sufficient 
+ *
+ */
+#include "wifi.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
+#include "esp_event.h"
+// check the header file first
+
+static const char *TAG = "scan";
+
+static void initialize_nvs(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret ==ESP_ERR_NVS_NO_FREE_PAGES  || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+}
+
+static void wifi_event_handler(void* arg, esp_event_base_t *event, int32_t event_id, void* event_data)
+{
+    switch(event_id)
+    {
+        case WIFI_EVENT_STA_CONNECTED:
+            printf("Connected \n");
+            break;  
+        case WIFI_EVENT_STA_DISCONNECTED:
+            printf("Disconnected \n");
+            break;
+        default:
+            printf("%ld \n",event_id);
+            break;
+    }
+}
+
+void init_wifi_event_loop(void)
+{
+    
+    ESP_ERROR_CHECK(esp_event_loop_create_default()); // creates an event catch/handle loop
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+}
+ // checks for the authentifcation mode, not really useful since you can pretty much find out on your own using 
+ // a phone or a computer.
+static void print_auth_mode(int authmode)
+{
+    switch (authmode) {
+    case WIFI_AUTH_OPEN:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_OPEN");
+        break;
+    case WIFI_AUTH_OWE:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_OWE");
+        break;
+    case WIFI_AUTH_WEP:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WEP");
+        break;
+    case WIFI_AUTH_WPA_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA_PSK");
+        break;
+    case WIFI_AUTH_WPA2_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_PSK");
+        break;
+    case WIFI_AUTH_WPA_WPA2_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA_WPA2_PSK");
+        break;
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_ENTERPRISE");
+        break;
+    case WIFI_AUTH_WPA3_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_PSK");
+        break;
+    case WIFI_AUTH_WPA2_WPA3_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_WPA3_PSK");
+        break;
+    default:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_UNKNOWN");
+        break;
+    }
+}
+
+void init_wifi(char mode , access_point_t ap )
+{
+    initialize_nvs();
+    ESP_ERROR_CHECK(esp_netif_init()); //initializes  the network stack
+    init_wifi_event_loop();
+    wifi_init_config_t cfg_default = WIFI_INIT_CONFIG_DEFAULT(); // default config to use when in station mode (Nobody cares since you can't have le funny SSID)
+    wifi_config_t cfg_ap = {
+        .ap = {
+        .ssid = WIFI_AP_SSID,
+        .password =WIFI_AP_PASSWORD,
+        .channel = WIFI_AP_CHANNEL,
+        .ssid_hidden = WIFI_AP_HIDDEN,
+        .max_connection = WIFI_AP_MAX_CONNECTIONS,
+        .beacon_interval = WIFI_AP_BEACON,   
+        },
+    };
+    wifi_config_t cfg_sta = {
+            .sta = {
+                .ssid = ap.ssid,
+                .password = ap.password,
+            },
+            };
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg_default));
+    switch(mode)
+    {
+        
+        case 's':
+        esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+        assert(sta_netif);
+
+
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA)); // set wifi to "sta" mode ie: a wifi station (eg: a laptop is a wifi station) 
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &cfg_sta));
+        ESP_ERROR_CHECK(esp_wifi_start()); // start the wifi according to the current configuration 
+        ESP_ERROR_CHECK(esp_wifi_connect());
+        case 'a':
+        
+        esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
+        assert(ap_netif);
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP)); 
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP,&cfg_ap));
+        ESP_ERROR_CHECK(esp_wifi_start()); // start the wifi according to the current configuration 
+        break;      
+    }
+
+}
+/* Requires the esp to be in station mode*/
+wifi_ap_record_register_t wifi_scan(void)
+{
+	
+    wifi_ap_record_register_t register_;
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE]; // a list containing informations about the  available networks (access points)
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
+
+    esp_wifi_scan_start(NULL, true); //starts scanning for networks, takes two arguments the config (set to NULL for defaults) and block ie: whether or not the scan will block the caller until it's done 
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    register_.ap_array = ap_info;
+    register_.ap_array_size = DEFAULT_SCAN_LIST_SIZE;
+    return register_; 
+}
---
+++ ep_wifi.h
@@ -0,0 +1,75 @@
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include <string.h>
+#include <stdbool.h>
+
+/*
+* inclusiong guard, it's standard pratctice to avoid header files from being included more than once
+* will save you a lot of headaches
+*/
+#ifndef MAIN_WIFI_H_
+
+#define MAIN_WIFI_H_
+/* macros for custom access point config */
+#define WIFI_AP_SSID "EDP445"
+#define WIFI_AP_PASSWORD "cupcake"
+#define WIFI_AP_CHANNEL 1 // Just use channel 1 no need to worry about interference or legal bullshit
+#define WIFI_AP_HIDDEN 0 // Don't hide the network, let the feds know you're there
+#define WIFI_AP_MAX_CONNECTIONS  12 // is it 12 + the Adoul or 11 + the Adoul ??
+#define WIFI_AP_BEACON 100 // Determines how often the beacon (access point) will advertise itself (share relevant info like the SSID AUTH_MODE ... etc)
+#define WIFI_AP_IP "192.168.4.1" // Just use the default, no need to get fancy
+#define WIFI_AP_GATEWAY "192.168.4.1" // ever visited 192.168.1.1 ? Yeah, that's what a gateway is.
+#define WIFI_AP_BW WIFI_BW_HT20 // shamelessly copied. But feel free to do your own research
+
+#endif
+
+typedef struct {
+    wifi_ap_record_t * ap_array;
+    uint8_t ap_array_size;
+} wifi_ap_record_register_t;
+
+/*
+* access point structure with 64 char limit on the password and 32 char limit on the ssid as defined by IEEE
+*/
+typedef struct {
+    char ssid[32];
+    char password[64];
+} access_point_t;
+
+#define DEFAULT_SCAN_LIST_SIZE 20
+/**
+* Initializes the wifi and ip event loop
+*/
+void init_wifi_event_loop(void);
+/**
+* event handler for wifi events, check esp_wifi_types.h for event types. For our purposes this handler only handles connect/disconnect events in station mode
+* @param arg data passed to the handler upon call (irrelevant)
+* @param event event base in our case it's WIFI_EVENT
+* @param event_id event specefic id
+* @param event_data
+*/
+static void wifi_event_handler(void* arg, esp_event_base_t *event, int32_t event_id, void* event_data);
+/*
+* Initializes non volatile storage
+*/
+static void initialize_nvs(void);
+/**
+*Identifies the authentification mode of an access point
+* @param authmode, authentification mode from predefined macros in esp_wifi.h
+*/
+static void print_auth_mode(int authmode);
+/**
+* Initialize wireless in either station or access point mode [s/a], should only be ran once
+* @param mode 's' for station and 'a' for access point
+* @param ap access point info for station mode
+*/
+void init_wifi(char mode, access_point_t ap);
+/*
+* Scans for access points and returns a wifi_ap_record_register_t pointer requires the wifi to be initialized in station mode
+*/
+wifi_ap_record_register_t wifi_scan(void);
+/*
+* Scans for access points and returns a wifi_ap_record_register_t pointer requires the wifi to be initialized in station mode
+*/
+
